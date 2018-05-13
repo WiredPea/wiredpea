31cc41adf09bec50b2f45cb65b13a3da91ffab4e
Noë Snaterse <noe@wiredpea.com>
2018-05-13 10:54:31 +0200

    Made a nicer font, and made an .eslintrc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
-"use strict";
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const log = require('fancy-log');
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var concat = require("gulp-concat");
-var uglify = require("gulp-uglify");
-var log = require("fancy-log");
-
-gulp.task("styles", function() {
-    log.info("Starting build");
-    return gulp.src("web/themes/custom/wiredpea/sass/**/*.scss", { verbose: true })
-        .pipe(sass().on("error", sass.logError))
-        .pipe(gulp.dest("web/themes/custom/wiredpea/css/"));
+gulp.task('styles', () => {
+  log.info('Starting build');
+  return gulp.src('web/themes/custom/wiredpea/sass/**/*.scss', { verbose: true })
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('web/themes/custom/wiredpea/css/'));
 });
 
-//Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+// Watch task
+gulp.task('default', () => {
+  gulp.watch('sass/**/*.scss', ['styles']);
 });
